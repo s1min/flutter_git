83b40f6774a0a125be31b5a3a63011c303f38e9c
raohonghua <raohonghua@foxmail.com>
2019-09-01 23:13:29 +0800

    fixed

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_git/i18n/app_localizations.dart';
 import 'package:flutter_git/views/widgets/app_bar_search.dart';
 
+import '../../common/model.dart';
+
 class HomePage extends StatefulWidget {
   HomePage({
     Key key,
@@ -15,23 +17,85 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: MediaQuery.removePadding(
+        removeTop: true,
         context: context,
-        child: AppBar(
-
-          leading: Builder(
-            builder: (context) {
-              return IconButton(
-                icon: Image.asset('assets/images/avatar_default.png', width: 40),
-                onPressed: () {
-                  widget.onPressedAppBarAvatar('123');
-                },
-              );
-            },
+        child: NotificationListener(
+          onNotification: (scrollNotification) {
+            print(scrollNotification);
+          },
+          child: ListView(
+            children: <Widget>[
+              // 头部搜索栏
+              Container(
+                height: 100,
+                padding: EdgeInsets.only(top: 40, right: 20, left: 20),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black54,
+                      offset: Offset(2.0,2.0),
+                      blurRadius: 4.0
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Flex(
+                      direction: Axis.horizontal,
+                      children: <Widget>[
+                        Expanded(
+                          flex: 1,
+                          child: Builder(builder: (context) {
+                            return IconButton(
+                              icon: Image.asset(
+                                'assets/images/avatar_default.png', 
+                                width: 32, 
+                                height: 32,
+                              ),
+                              onPressed: () {
+                                Scaffold.of(context).openDrawer();
+                                widget.onPressedAppBarAvatar(context);
+                              },
+                            );
+                          })
+                        ),
+                        Expanded( 
+                          flex: 4,
+                          child: Container(
+                            height: 28,
+                            padding: EdgeInsets.only(left: 8, right: 8),
+                            alignment: Alignment.centerLeft,
+                            decoration: BoxDecoration(
+                              border: Border.all(width: 0),
+                              borderRadius: BorderRadius.all(Radius.circular(3)),
+                              color: Colors.white,
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Text(
+                                  '搜索',
+                                  style: TextStyle(color: Color(0xff6a737d)),
+                                ),
+                                Icon(
+                                  Icons.search,
+                                  color: Color(0xff6a737d),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
